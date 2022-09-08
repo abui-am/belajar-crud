0ebae22b7c02859e657aa3654cc863cb9bdc59e7
Abui <adjiem31@gmail.com>
2022-09-09 00:11:10 +0700

    feat: add formik

--- src/components/form/FormCreateUser.jsx
+++ src/components/form/FormCreateUser.jsx
@@ -1,8 +1,7 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Form as FormikForm, Formik } from 'formik';
+import React, { useEffect } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { postUserApi, putUserApi } from '../../api/user';
 import {
   getDetailUser,
   postUser,
@@ -10,52 +9,38 @@ import {
   selectUserDetail,
   selectUserDetailStatus,
 } from '../../redux/feature/usersSlice';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string().required('First name wajib di masukan'),
+  lastName: Yup.string().required('First name wajib di masukan'),
+  email: Yup.string().email('Email tidak valid'),
+  avatar: Yup.string().url('Url tidak valid'),
+  gender: Yup.string()
+    .required('Gender wajib diisi')
+    .equals(['male', 'female']),
+});
 
 const FormCreateUser = ({ editId }) => {
   const userDetail = useSelector(selectUserDetail);
   const userDetailStatus = useSelector(selectUserDetailStatus);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [avatar, setAvatar] = useState('');
-  const [email, setEmail] = useState('');
-  const [errors, setError] = useState({});
+  const initialValues = editId
+    ? {
+        lastName: userDetail.last_name,
+        firstName: userDetail.first_name,
+        email: userDetail.email,
+        avatar: userDetail.avatar,
+        gender: '',
+      }
+    : {
+        lastName: '',
+        firstName: '',
+        email: '',
+        avatar: '',
+        gender: '',
+      };
+
   const dispatch = useDispatch();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (firstName || lastName) {
-      try {
-        const payload = {
-          first_name: firstName,
-          last_name: lastName,
-          avatar,
-          email,
-        };
-        if (editId) {
-          dispatch(putUser({ editId, data: payload }));
-        } else {
-          dispatch(postUser(payload));
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    } else {
-      if (!lastName) {
-        setError((error) => ({
-          ...error,
-          lastName: 'Last name wajib di masukan',
-        }));
-      }
-
-      if (!firstName) {
-        setError((error) => ({
-          ...error,
-          firstName: 'First name wajib di masukan',
-        }));
-      }
-    }
-  };
 
   useEffect(() => {
     if (editId) {
@@ -63,71 +48,125 @@ const FormCreateUser = ({ editId }) => {
     }
   }, [editId]);
 
-  useEffect(() => {
-    if (userDetail) {
-      setFirstName(userDetail.first_name);
-      setLastName(userDetail.last_name);
-      setAvatar(userDetail.avatar);
-      setEmail(userDetail.email);
-    }
-  }, [userDetail]);
-
   if (userDetailStatus === 'loading') {
     return <div>Loading...</div>;
   }
   return (
     <Container>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className='mb-3'>
-          <Form.Label>First Name *</Form.Label>
-          <Form.Control
-            type='text'
-            value={firstName}
-            placeholder='Masukkan first name'
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-          />
-          <small style={{ color: 'red' }}>{errors.firstName}</small>
-        </Form.Group>
-        <Form.Group className='mb-3'>
-          <Form.Label>Last Name *</Form.Label>
-          <Form.Control
-            type='text'
-            value={lastName}
-            placeholder='Masukkan last name'
-            onChange={(e) => {
-              setLastName(e.target.value);
-            }}
-          />
-          <small style={{ color: 'red' }}>{errors.lastName}</small>
-        </Form.Group>
-        <Form.Group className='mb-3'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type='email'
-            value={email}
-            placeholder='Masukkan email'
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Form.Group className='mb-3'>
-          <Form.Label>Avatar url</Form.Label>
-          <Form.Control
-            type='text'
-            value={avatar}
-            placeholder='Masukkan avatar url'
-            onChange={(e) => {
-              setAvatar(e.target.value);
-            }}
-          />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
+      <Formik
+        enableReinitialize
+        validationSchema={validationSchema}
+        initialValues={initialValues}
+        onSubmit={(value) => {
+          try {
+            const payload = {
+              first_name: value.firstName,
+              last_name: value.lastName,
+              avatar: value.avatar,
+              email: value.email,
+              gender: value.gender,
+            };
+            if (editId) {
+              dispatch(putUser({ editId, data: payload }));
+            } else {
+              dispatch(postUser(payload));
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        }}
+      >
+        {(formikProps) => {
+          console.log(formikProps.values);
+          return (
+            <FormikForm>
+              <Form.Group className='mb-3'>
+                <Form.Label>First Name *</Form.Label>
+                <Form.Control
+                  type='text'
+                  name='firstName'
+                  placeholder='Masukkan first name'
+                  value={formikProps.values.firstName}
+                  onChange={formikProps.handleChange}
+                />
+                {formikProps.errors.firstName &&
+                  formikProps.touched.firstName && (
+                    <small style={{ color: 'red' }}>
+                      {formikProps.errors.firstName}
+                    </small>
+                  )}
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Last Name *</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Masukkan last name'
+                  name='lastName'
+                  value={formikProps.values.lastName}
+                  onChange={formikProps.handleChange}
+                />
+                {formikProps.errors.lastName &&
+                  formikProps.touched.lastName && (
+                    <small style={{ color: 'red' }}>
+                      {formikProps.errors.lastName}
+                    </small>
+                  )}
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type='email'
+                  placeholder='Masukkan email'
+                  name='email'
+                  value={formikProps.values.email}
+                  onChange={formikProps.handleChange}
+                />
+                {formikProps.errors.email && formikProps.touched.email && (
+                  <small style={{ color: 'red' }}>
+                    {formikProps.errors.email}
+                  </small>
+                )}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Jenis Kelamin</Form.Label>
+                <Form.Select
+                  aria-label='Default select example'
+                  onChange={formikProps.handleChange}
+                  name='gender'
+                >
+                  <option value=''>Pilih jenis kelamin</option>
+
+                  <option value='male'>Pria</option>
+                  <option value='female'>Wanita</option>
+                </Form.Select>
+                {formikProps.errors.gender && formikProps.touched.gender && (
+                  <small style={{ color: 'red' }}>
+                    {formikProps.errors.gender}
+                  </small>
+                )}
+              </Form.Group>
+              <Form.Group className='mb-3'>
+                <Form.Label>Avatar url</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Masukkan avatar url'
+                  name='avatar'
+                  value={formikProps.values.avatar}
+                  onChange={formikProps.handleChange}
+                />
+                {formikProps.errors.avatar && formikProps.touched.avatar && (
+                  <small style={{ color: 'red' }}>
+                    {formikProps.errors.avatar}
+                  </small>
+                )}
+              </Form.Group>
+              <Button variant='primary' type='submit'>
+                Submit
+              </Button>
+            </FormikForm>
+          );
+        }}
+      </Formik>
     </Container>
   );
 };
